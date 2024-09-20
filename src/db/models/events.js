bd0413aa2707e5166e4eaf96cfc4920c17e8c9a5
Olena-Ishchenko <elena.dubovalova@gmail.com>
2024-09-20 10:41:01 +0300

    add bd

---
+++ src/db/models/events.js
@@ -0,0 +1,25 @@
+import { model, Schema } from 'mongoose';
+
+const eventsSchema = new Schema(
+    {
+        title: {
+            type: String,
+            require: true,
+        },
+        description: {
+            type: String,
+            require: true,
+        },
+        event_date: {
+            type: Date,
+            require: true,
+        },
+        organizer: {
+            type: String,
+            require: true,
+        },
+
+    }
+);
+
+export const EventsCollection = model('events', eventsSchema);
